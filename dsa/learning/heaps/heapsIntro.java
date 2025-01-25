e167273a9eef757086652c45628144e72e1c677f
satvik nanda <satvik.nanda@gmail.com>
2025-01-25 13:22:19 +0530

    insertion in heap

--- dsa/learning/heaps/heapsIntro.java
+++ dsa/learning/heaps/heapsIntro.java
@@ -1,5 +1,19 @@
 package dsa.learning.heaps;
+import java.util.*;
 
 public class heapsIntro {
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to heaps!");
+        Heap<Integer> heap = new Heap<>();
+        
+
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(22);
+        heap.insert(89);
+        heap.insert(76);
+
+        ArrayList list = heap.heapSort();
+        System.out.println(list);
+    }
 }
